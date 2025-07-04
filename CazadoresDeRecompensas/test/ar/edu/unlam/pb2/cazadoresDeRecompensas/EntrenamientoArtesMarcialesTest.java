d1f63209fcb36823a38d758a023495a6b5c820d9
JuanMM27 <juanartin9@gmail.com>
2025-07-04 18:16:17 -0300

    Separacion de casos de prueba para mayor orden

---
+++ CazadoresDeRecompensas/test/ar/edu/unlam/pb2/cazadoresDeRecompensas/EntrenamientoArtesMarcialesTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.cazadoresDeRecompensas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntrenamientoArtesMarcialesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
